114e6023b306c0831d557be5d826cc5c952e238d
John <ugborjohn@gmail.com>
2021-06-06 13:01:15 +0100

    Update Store.js
    changed api from wesapi to mediastack api

--- src/Store.js
+++ src/Store.js
@@ -17,10 +17,10 @@ export class NewsStore{
 	async loadNewsArticles(){
 
 		
-			const response = await fetch("https://newsapi.org/v2/top-headlines?country=ng&apiKey=cad9015fba4e455ead21f54b6de73761");
+			const response = await fetch("http://api.mediastack.com/v1/news?access_key=70fbdb9d286e3abc9a7fb88db2dc8d60");
 			const responseInJson = await response.json();
 			runInAction(()=>{
-				this.articles =responseInJson.articles;
+				this.articles =responseInJson.data;
 
 			})	
 	
